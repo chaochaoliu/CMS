cba9de62c7ae870b1368332657cb23f5f2be3f65
Kevin <liuchao200240@gmail.com>
2016-08-16 11:42:41 -0700

    change main page to avoid nil objects

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -8,17 +8,23 @@ class MainController < ApplicationController
     @my_approved_events = []
     @event_sermons = []
     @my_approved_event_registrations = EventRegistration.where(user_id: current_user.id, status: '2')
-    @my_approved_event_registrations.each do |my_approved_event_registration|
-      @my_approved_events << my_approved_event_registration.event
+    
+    unless @my_approved_event_registrations.nil?
+      @my_approved_event_registrations.each do |my_approved_event_registration|
+        @my_approved_events << my_approved_event_registration.event
+      end
     end
 
-    @my_approved_events.each do |approved_event|
-        approved_event.event_sermons.each do |event_sermon|
-           @event_sermons << event_sermon
-       end
+    unless @my_approved_events.nil?
+      @my_approved_events.each do |approved_event|
+          approved_event.event_sermons.each do |event_sermon|
+             @event_sermons << event_sermon
+         end
+      end
     end
+
     @sermons = Sermon.all
     @events = @event_sermons + @sermons
-  end
-  
+    
+  end  
 end
